fac18311c9fd74d8357d825171c75c6db9699768
Holly Wilkalis and Sailor Winkelman <sailor@>
2018-01-04 11:02:33 -0800

    Create project directories. Install gulp, create gulpfile with packages to concat js interface files.

---
+++ gulp-triangle/gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require("gulp");
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
+
+gulp.task('concatInterface', function(){
+  return gulp.src(['./js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
+
+});
