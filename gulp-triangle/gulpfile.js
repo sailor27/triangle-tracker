9385552369c375b93960282e6083c3f72d46bc14
Holly Wilkalis and Sailor Winkelman <sailor@>
2018-01-04 11:34:46 -0800

    Add gulp tasks to clean, build and serve.

--- gulp-triangle/gulpfile.js
+++ gulp-triangle/gulpfile.js
@@ -2,10 +2,50 @@ var gulp = require("gulp");
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var utilities = require('gulp-util');
+var del = require('del');
+var buildProduction = utilities.env.production;
+var browserSync = require('browser-sync').create();
 
+//concatenate js interface files, pipe to tmp
 gulp.task('concatInterface', function(){
   return gulp.src(['./js/*-interface.js'])
     .pipe(concat('allConcat.js'))
     .pipe(gulp.dest('./tmp'));
-
 });
+
+gulp.task('jsBrowserify',['concatInterface'], function(){
+  return browserify({ entries: ['./tmp/allConcat.js'] })
+  //babelify goes here
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('minifyScripts', ['jsBrowserify'], function(){
+  return gulp.src("./build/js/app.js")
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('clean', function(){
+  return del(['build'], 'tmp')
+});
+
+gulp.task('build', ['clean'], function() {
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+    //start bower and cssBuild here
+  }
+});
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
