7c78191ca4d30d5d1cf03edc3133d009c1d4d4ab
Holly Wilkalis and Sailor Winkelman <sailor@>
2018-01-04 14:27:53 -0800

    Install packages for testing (jasmine and karma), install bower, jquery and bootstrap, write gulp tasks to serve, put jquery and bootstrap into min files, watch files and restart tasks when they are changed

--- gulp-triangle/gulpfile.js
+++ gulp-triangle/gulpfile.js
@@ -7,7 +7,30 @@ var utilities = require('gulp-util');
 var del = require('del');
 var buildProduction = utilities.env.production;
 var browserSync = require('browser-sync').create();
+var jshint = require('gulp-jshint');
+var watchify = require('watchify');
+var lib = require('bower-files')({
+  "overrides":{
+    "bootstrap":{
+      "main":[
+        "less/bootstrap.less",
+        "dist/css/bootstrap.css",
+        "dist/js/bootstrap.js"
+      ]
+    }
+  }
+});
+// var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
+//linter to run on all npfiles in js folder//
+gulp.task('jshint', function(){
+  return gulp.src(['js/*.js'])
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'));
+});
+
+// BUILD~~~~~~~~~~~~~~~~~~
 //concatenate js interface files, pipe to tmp
 gulp.task('concatInterface', function(){
   return gulp.src(['./js/*-interface.js'])
@@ -29,6 +52,21 @@ gulp.task('minifyScripts', ['jsBrowserify'], function(){
     .pipe(gulp.dest('./build/js'));
 });
 
+gulp.task('bowerJS', function () {
+  return gulp.src(lib.ext('js').files)
+    .pipe(concat('vendor.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('bowerCSS', function(){
+  return gulp.src(lib.ext('css').files)
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('bower', ['bowerJS', 'bowerCSS']);
+
 gulp.task('clean', function(){
   return del(['build'], 'tmp')
 });
@@ -40,8 +78,11 @@ gulp.task('build', ['clean'], function() {
     gulp.start('jsBrowserify');
     //start bower and cssBuild here
   }
+  gulp.start('bower');
+  // gulp.start('cssBuild');
 });
 
+//SERVE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 gulp.task('serve', function() {
   browserSync.init({
     server: {
@@ -49,3 +90,37 @@ gulp.task('serve', function() {
       index: "index.html"
     }
   });
+    //watch tasks go here (js, bower, html, sass)
+    //subtasks for gulp watch to reload the for each watch task change go here
+  gulp.watch(['js/*.js'], ['jsBuild']);
+  gulp.watch(['bower.json'], ['bowerBuild']);
+  gulp.watch(['*.html'], ['htmlBuild']);
+  // gulp.watch(["scss/*.scss", "scss/**/*.scss"], ['cssBuild']);
+});
+
+gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function() {
+  browserSync.reload();
+});
+
+gulp.task('bowerBuild', ['bower'], function(){
+  browserSync.reload();
+})
+
+gulp.task('htmlBuild', function(){
+  browserSync.reload();
+});
+
+//map changes to .scss files to .css files
+// gulp.task('cssBuild', function(){
+//   return gulp.src('./scss/*.scss')
+//     .pipe(sourcemaps.init())
+//     .pipe(sass().on('error'), sass.logError)
+//     .pipe(sourcemaps.write())
+//     .pipe(gulp.dest('./build/css'))
+//     .pipe(browserSync.stream());
+// });
+
+
+//TEST ~~~~~~~~~~~~~~~~~~~~~~~~
+
+//install jasmine, initialize, install watchify, install karma and karma, etc
